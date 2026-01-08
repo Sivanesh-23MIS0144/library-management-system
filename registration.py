48bfc2a173e9b58729369c6c724213019aa5b037
Sivanesh P <sivanesh.p2023@vitstudent.ac.in>
2026-01-08 11:24:30 +0530

    Notification message added

--- registration.py
+++ registration.py
@@ -1,5 +1,6 @@
 def register_student(student_id, name):
     print(f"Registering student: {name} ({student_id})")
+    print("Student is successfully registered")
     return True
 
 register_student("23MIS0144","Sivanesh")
